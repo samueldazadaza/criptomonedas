6b4a4175bd5520913a03137e79d8e4876d38d1d8
Samuel Daza <samueldazadaza@gmail.com>
2021-05-31 13:59:34 -0500

    renderizado de listas con v-for

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ new Vue({
     return {
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 20
+      changePercent: 20,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 11456],
+
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 11456 },
+      ]
     }
   }
 })
